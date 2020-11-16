d72b5f5719c3d62f9a090b5676f701846dc1a53e
bruno-wolf <bruno.m.lobato@gmail.com>
2020-11-16 22:23:00 +0000

    added repositories list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
@@ -12,6 +13,17 @@ const Dashboard: React.FC = () => {
         <input type="text" placeholder="Escreva o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars2.githubusercontent.com/u/57861255?s=460&u=fab67d1be099c5eebb9bcd5d51c95bd86070f8b4&v=4" alt="Wolf" />
+          <div>
+            <strong>bruno-wolf/onlive</strong>
+            <p>A website for booking and see online live music concerts</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
+
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
@@ -42,5 +43,58 @@ export const Form = styled.form`
     &:hover {
       background: ${shade(.2, '#04d361')}
     }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+  
+  a {
+    background: #fff;
+    color: #a8a8b3;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    text-decoration:none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 300ms ease-in-out;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+    
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+    
+    div {
+      margin-left: 16px;
+      
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+  
+        p {
+          font-size: 18px;
+          color: #a8a8b3;
+          margin-top: 4px;
+        }
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+
   }
 `;
